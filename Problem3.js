d997415bd4f9a746ca79c265c8920d82110e6b4a
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-08-18 20:36:41 +0600

    solve probelm4

---
+++ Problem3.js
@@ -0,0 +1,13 @@
+const vowels = (input) => {
+
+    let max = input[0];
+    for (let i of input) {
+        if (i > max) {
+            max = i
+        }
+    }
+
+    console.log(max)
+}
+
+vowels([5, 1, 9, 3])
