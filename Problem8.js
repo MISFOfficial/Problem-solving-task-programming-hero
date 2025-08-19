90df71e31776b3cc6f4409a8f70a0c31752f7072
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-08-19 10:46:00 +0600

    problem 8 solved

---
+++ Problem8.js
@@ -0,0 +1,12 @@
+// Write a function that capitalizes the first letter of each word in a string.
+
+const word = (input) => {
+
+    const newWord = input.split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+
+    console.log(newWord)
+}
+
+word("hello world")
